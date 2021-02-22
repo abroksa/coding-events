b91f5453d2239d8474a3bf7abda8de755d9d0c8e
abroksa <slipstitched@gmail.com>
2021-02-22 01:58:50 -0600

    Added "No Events!" message for blank list

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,13 +14,16 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Taste of St Louis");
-        events.add("Lou Fest");
-        events.add("Shakespeare in the Park");
-        events.add("Turkey Trot");
+//        List<String> events = new ArrayList<>();
+//        events.add("Taste of St Louis");
+//        events.add("Lou Fest");
+//        events.add("Shakespeare in the Park");
+//        events.add("Turkey Trot");
+
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -27,5 +32,12 @@ public class EventController {
     public String renderCreateEventForm() {
         return "create";
         //note: previous line, Chris had path = events/create and I got an error??
+    }
+
+    @PostMapping("create")
+    public String createEvents(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:";
+
     }
 }
